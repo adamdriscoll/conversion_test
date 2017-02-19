b577177ea9dee69dd6fcdf96a4b836593d98e3d9
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2017-02-19 18:40:48 +0700

    Automated Conversion

---
+++ CSharp/10/PowerShell.ps1
@@ -0,0 +1,3 @@
+function ET_Main{param([string]$permissions)$InitializeComponent()
+$Status.Text=$permissions
+}Edit on GitHub at 'https://github.com/adamdriscoll/conversion_test/blob/master/CSharp/10/PowerShell.ps1'.
